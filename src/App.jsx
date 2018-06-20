eae9747b5ce8bbb83113b673dcd669a98328ac22
Hanocha <saiki@feedforce.jp>
2018-06-20 20:16:14 +0900

    Get todos from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
+import axios from "axios";
 import { TodoList } from "./TodoList";
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [],
+  };
+
+  componentDidMount() {
+    axios.get(
+      "/todos",
+      {
+        baseURL: "https://virtserver.swaggerhub.com/kielze/TODO-API/1.0.0",
+        headers: {
+          'Accept': 'application/json'
+        },
+      },
+    )
+      .then(response => {
+        console.log(response);
+        console.log(this.state);
+        this.setState({
+          todos: response.data.map(todo => {
+            return ({ id: todo.id, body: todo.title });
+          })
+        })
+      });
+  }
+
   render() {
     return (
-      <TodoList items={[
-        { id: 1, body: "test1" },
-        { id: 2, body: "test2" },
-      ]} />
+      <TodoList items={this.state.todos} />
     );
   }
 }
